bd7e83415d29946349008b552251d5d8b359b0cd
Michael Faber <m.warrior_4066@icloud.com>
2026-04-22 01:04:19 -0700

    Change R2Explorer to be writable

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,8 +1,7 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
-export default R2Explorer({
-	readonly: true,
+export default R2Explorer({ readonly: false });
 	// basicAuth: {
 	// 	username: "username",
 	// 	password: "password",
